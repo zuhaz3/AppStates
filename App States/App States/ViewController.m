918a3ee73e2998319a12b85d3b7a0f58e3924401
Zuhayeer Musa <zuhaz3@gmail.com>
2015-06-27 15:52:33 -0700

    filled in user cells

--- App States/App States/ViewController.m
+++ App States/App States/ViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "ViewController.h"
+#import "UIImageView+WebCache.h"
+#import "UserTableViewCell.h"
 
 @interface ViewController ()
 
@@ -17,6 +19,23 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.tableView.tableFooterView = [[UIView alloc] initWithFrame:CGRectZero];
+    
+    // Initialize refresh control
+    if (self.refreshControl == nil) {
+        UIRefreshControl *refreshControl = [[UIRefreshControl alloc] init];
+        [refreshControl addTarget:self action:@selector(onRefresh:) forControlEvents:UIControlEventValueChanged];
+    
+        refreshControl.tintColor = [UIColor darkGrayColor];
+        
+        [self.tableView addSubview:refreshControl];
+        [self.tableView sendSubviewToBack:refreshControl];
+        self.refreshControl = refreshControl;
+    }
+}
+
+-(void)onRefresh:(id)sender {
+    [self.refreshControl endRefreshing];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -28,11 +47,35 @@
     
 }
 
-- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
-    return 45.0;
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *CellIdentifier = @"UserTableCell";
+    
+    UserTableViewCell *cell = [tableView
+                               dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil) {
+        cell = [[UserTableViewCell alloc]
+                initWithStyle:UITableViewCellStyleDefault
+                reuseIdentifier:CellIdentifier];
+    }
+    cell.profileImage.layer.cornerRadius = 29;
+    cell.profileImage.layer.masksToBounds = YES;
+    cell.profileImage.layer.borderWidth = 0;
+    [cell.profileImage sd_setImageWithURL:[NSURL URLWithString:@"https://pbs.twimg.com/profile_images/514108659660759040/PwcLTe3q_400x400.jpeg"]
+                         placeholderImage:[UIImage imageNamed:@"profile"] options:SDWebImageRefreshCached];
+    
+    cell.userName.text = @"Emma Watson";
+    
+    return cell;
 }
 
-- table
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return 75.0;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 5;
+}
+
 
 
 
