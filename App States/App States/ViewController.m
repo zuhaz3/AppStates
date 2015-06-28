0ce278e51da9c7cb16efb6f38caf59347ca068b2
Zuhayeer Musa <zuhaz3@gmail.com>
2015-06-28 02:26:30 -0700

    vertical only/uploading code merge

--- App States/App States/ViewController.m
+++ App States/App States/ViewController.m
@@ -8,9 +8,14 @@
 
 #import "ViewController.h"
 #import "UIImageView+WebCache.h"
+#import "SVHTTPRequest.h"
 #import "UserTableViewCell.h"
+#import <AssetsLibrary/AssetsLibrary.h>
+#import "AFNetworking.h"
 
 @interface ViewController ()
+
+@property (nonatomic) UIImage *selectedImage;
 
 @end
 
@@ -32,10 +37,21 @@
         [self.tableView sendSubviewToBack:refreshControl];
         self.refreshControl = refreshControl;
     }
+    
+    [self onRefresh:self];
 }
 
 -(void)onRefresh:(id)sender {
     [self.refreshControl endRefreshing];
+}
+
+- (IBAction)addImage:(id)sender {
+    UIActionSheet *popup = [[UIActionSheet alloc] initWithTitle:@"Select photo upload option:" delegate:self cancelButtonTitle:@"Cancel" destructiveButtonTitle:nil otherButtonTitles:
+                            @"Use Last Taken",
+                            @"Choose Photo",
+                            nil];
+    popup.tag = 1;
+    [popup showInView:[UIApplication sharedApplication].keyWindow];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -44,7 +60,13 @@
 }
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    [[UIApplication sharedApplication] openURL:
+     [NSURL URLWithString: @"soundcloud://tracks:125636702"]];
+    //      @"music://"]];
+    //      @"http://phobos.apple.com/WebObjects/MZStore.woa/wa/viewAlbum?i=156093464&id=156093462&s=143441"]];
+    //      @"spotify:track:2QO8cnnFW6khDuAuUAySeV#19000"]];
     
+    [self.tableView deselectRowAtIndexPath:[self.tableView indexPathForSelectedRow] animated:YES];
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
@@ -57,13 +79,13 @@
                 initWithStyle:UITableViewCellStyleDefault
                 reuseIdentifier:CellIdentifier];
     }
-    if(indexPath.row == 1){
+    if(indexPath.row % 4 == 1){
         cell.opacityFilter.backgroundColor = [UIColor colorWithRed:0.902 green:0.494 blue:0.133 alpha:0.75];
     }
-    else if (indexPath.row == 2){
+    else if (indexPath.row % 4 == 2){
         cell.opacityFilter.backgroundColor = [UIColor colorWithRed:0.102 green:0.737 blue:0.612 alpha:0.75];
     }
-    else if (indexPath.row == 3){
+    else if (indexPath.row % 4 == 3){
         cell.opacityFilter.backgroundColor = [UIColor colorWithRed:0.608 green:0.349 blue:0.714 alpha:0.75];
     }
     cell.uploadersName.text = @"Danish Shaik";
@@ -74,7 +96,7 @@
     cell.backgroundImageView.layer.cornerRadius = 3;
     cell.backgroundImageView.layer.masksToBounds = YES;
     [cell.backgroundImageView sd_setImageWithURL:[NSURL URLWithString:@"http://www.noiseporn.com/wp-content/uploads/spotifyroyalties.jpg"]
-                                placeholderImage:[UIImage imageNamed:@"profile"] options:SDWebImageRefreshCached];
+                                placeholderImage:nil options:SDWebImageRefreshCached];
     cell.roundedView.layer.cornerRadius = 3;
     cell.profileImage.layer.cornerRadius = 22;
     cell.profileImage.layer.masksToBounds = YES;
@@ -84,6 +106,8 @@
     
     cell.userName.text = @"No Church in the Wild, Kanye West";
     cell.userName.textColor = [UIColor whiteColor];
+    
+    cell.selectionStyle = UITableViewCellSelectionStyleNone;
     
     return cell;
 }
@@ -95,6 +119,106 @@
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     return 5;
 }
+
+#pragma mark - UIImagePickerControllerDelegate
+
+// This method is called when an image has been chosen from the library or taken from the camera.
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
+{
+    UIImage *image = [info valueForKey:UIImagePickerControllerOriginalImage];
+    self.selectedImage = image;
+    [self uploadPhotoToServer:image];
+    [self dismissViewControllerAnimated:YES completion:NULL];
+}
+
+
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker
+{
+    [self dismissViewControllerAnimated:YES completion:NULL];
+}
+
+#pragma mark UIActionSheetDelegate
+
+- (void)actionSheet:(UIActionSheet *)popup clickedButtonAtIndex:(NSInteger)buttonIndex {
+    
+    switch (popup.tag) {
+        case 1: {
+            switch (buttonIndex) {
+                case 0:
+                    [self getLastTaken];
+                    break;
+                case 1:
+                    [self popUpImagePicker];
+                    break;
+            }
+            break;
+        }
+        default:
+            break;
+    }
+}
+
+-(void)getLastTaken {
+    ALAssetsLibrary *assetsLibrary = [[ALAssetsLibrary alloc] init];
+    [assetsLibrary enumerateGroupsWithTypes:ALAssetsGroupSavedPhotos
+                                 usingBlock:^(ALAssetsGroup *group, BOOL *stop) {
+                                     if (nil != group) {
+                                         // be sure to filter the group so you only get photos
+                                         [group setAssetsFilter:[ALAssetsFilter allPhotos]];
+                                         
+                                         if (group.numberOfAssets > 0) {
+                                             [group enumerateAssetsAtIndexes:[NSIndexSet indexSetWithIndex:group.numberOfAssets - 1]
+                                                                     options:0
+                                                                  usingBlock:^(ALAsset *result, NSUInteger index, BOOL *stop) {
+                                                                      if (nil != result) {
+                                                                          ALAssetRepresentation *repr = [result defaultRepresentation];
+                                                                          // this is the most recent saved photo
+                                                                          UIImage *img = [UIImage imageWithCGImage:[repr fullResolutionImage]];
+                                                                          self.selectedImage = img;
+                                                                          [self uploadPhotoToServer:img];
+                                                                          // we only need the first (most recent) photo -- stop the enumeration
+                                                                          *stop = YES;
+                                                                      }
+                                                                  }];
+                                         }
+                                     }
+                                     
+                                     *stop = NO;
+                                 } failureBlock:^(NSError *error) {
+                                     NSLog(@"error: %@", error);
+                                 }];
+}
+
+-(void)popUpImagePicker {
+    UIImagePickerController *imagePickerController = [[UIImagePickerController alloc] init];
+    imagePickerController.modalPresentationStyle = UIModalPresentationCurrentContext;
+    imagePickerController.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    imagePickerController.delegate = self;
+    [self presentViewController:imagePickerController animated:YES completion:nil];
+}
+
+-(void)uploadPhotoToServer:(UIImage *)img {
+    
+    if (img != nil) {
+        
+        NSData *imageData = UIImageJPEGRepresentation(img, 0.5);
+        
+        AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+        
+        [manager POST:@"http://45.55.12.167:5000/upload" parameters:nil constructingBodyWithBlock:^(id<AFMultipartFormData> formData) {
+            [formData appendPartWithFileData:imageData
+                                        name:@"file"
+                                    fileName:@"file.jpg" mimeType:@"image/jpeg"];
+            
+            // etc.
+        } success:^(AFHTTPRequestOperation *operation, id responseObject) {
+            NSLog(@"Response: %@", responseObject);
+        } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+            NSLog(@"Error: %@", error);
+        }];
+    }
+}
+
 
 
 
