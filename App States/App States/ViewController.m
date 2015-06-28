b0a4e77360acf4472e4e67477778bb53185bad57
Zuhayeer Musa <zuhaz3@gmail.com>
2015-06-28 00:42:10 -0700

    SVHTTPRequest attempt

--- App States/App States/ViewController.m
+++ App States/App States/ViewController.m
@@ -178,55 +178,17 @@
 -(void)uploadPhotoToServer:(UIImage *)img {
     
     if (img != nil) {
-        NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:[NSURL URLWithString:@"http://45.55.12.167:5000/upload/"]];
         
         NSData *imageData = UIImageJPEGRepresentation(img, 1.0);
         
-        [request setCachePolicy:NSURLRequestReloadIgnoringLocalCacheData];
-        [request setHTTPShouldHandleCookies:NO];
-        [request setTimeoutInterval:60];
-        [request setHTTPMethod:@"POST"];
-        
-        NSString *boundary = @"unique-consistent-string";
-        
-        // set Content-Type in HTTP header
-        NSString *contentType = [NSString stringWithFormat:@"multipart/form-data; boundary=%@", boundary];
-        [request setValue:contentType forHTTPHeaderField: @"Content-Type"];
-        
-        // post body
-        NSMutableData *body = [NSMutableData data];
-        
-        // add params (all params are strings)
-        [body appendData:[[NSString stringWithFormat:@"--%@\r\n", boundary] dataUsingEncoding:NSUTF8StringEncoding]];
-        [body appendData:[[NSString stringWithFormat:@"Content-Disposition: form-data; name=%@\r\n\r\n", @"imageCaption"] dataUsingEncoding:NSUTF8StringEncoding]];
-        [body appendData:[[NSString stringWithFormat:@"%@\r\n", @"Some Caption"] dataUsingEncoding:NSUTF8StringEncoding]];
-        
-        // add image data
-        if (imageData) {
-            [body appendData:[[NSString stringWithFormat:@"--%@\r\n", boundary] dataUsingEncoding:NSUTF8StringEncoding]];
-            [body appendData:[[NSString stringWithFormat:@"Content-Disposition: form-data; name=%@; filename=file.jpg\r\n", @"file"] dataUsingEncoding:NSUTF8StringEncoding]];
-            [body appendData:[@"Content-Type: image/jpeg\r\n\r\n" dataUsingEncoding:NSUTF8StringEncoding]];
-            [body appendData:imageData];
-            [body appendData:[[NSString stringWithFormat:@"\r\n"] dataUsingEncoding:NSUTF8StringEncoding]];
-        }
-        
-        [body appendData:[[NSString stringWithFormat:@"--%@--\r\n", boundary] dataUsingEncoding:NSUTF8StringEncoding]];
-        
-        // setting the body of the post to the reqeust
-        [request setHTTPBody:body];
-        
-        // set the content-length
-        NSString *postLength = [NSString stringWithFormat:@"%d", [body length]];
-        [request setValue:postLength forHTTPHeaderField:@"Content-Length"];
-        
-        
-        [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue currentQueue] completionHandler:^(NSURLResponse *response, NSData *data, NSError *error) {
-            if(data.length > 0)
-            {
-                //success
-                NSLog(@"RESPONSE %@", response);
-            }
-        }];
+        [SVHTTPRequest POST:@"http://45.55.12.167:5000/upload/"
+                 parameters:[NSDictionary dictionaryWithObjectsAndKeys:imageData, @"file", nil]
+                   progress:^(float progress) {
+                       
+                   }
+                 completion:^(id response, NSHTTPURLResponse *urlResponse, NSError *error) {
+                     
+                 }];
     }
 }
 
